70be50b683024fd2d37a246c8c591cea2efa4269
Brent Cameron <bjcameron@gmail.com>
2020-05-01 10:25:02 -0600

    added a stack class which will be used for pushing all the operations/numbers into

--- Calculator.js
+++ Calculator.js
@@ -7,7 +7,48 @@ function Display() {
 }
 
 function ButtonPress (whichButton){
+    if(isNaN(whichButton)){
+        //then its not a number...
+    }else {
+        //it is a number and should be concatenated to the previous number if there are any.
+    }
     console.log(whichButton);
+}
+
+//this is where ill push all the new button press info
+//once we see an '=' then we will have to evaluate the elements of the stack using BEDMAS
+class Stack {
+    constructor() {
+        this.data = [];
+        this.top = 0;
+    }
+    push (element) {
+        this.data[this.top] = element;
+        this.top = this.top + 1;
+    }
+    length() {
+        return this.top;
+    }
+    peek() {
+        return this.data[this.top -1];
+    }
+    isEmpty() {
+        return this.top === 0;
+    }
+    pop () {
+        if( this.isEmpty() === false){
+            this.top = this.top -1; 
+            return this.data.pop();
+        }
+    }
+    print() {
+        let top = this.top -1;
+
+        while( top >= 0){
+            console.log(this.data[top]);
+            top --;
+        }
+    }
 }
 
 function Addition (a, b) {
@@ -24,6 +65,11 @@ function Division (a, b) {
 
 function Multiplication (a, b){
     return (parseInt(a) * parseInt(b));
+}
+
+function Clear () {
+    let buttonStack = new Stack ();
+    return(buttonStack);
 }
 
 Display();
