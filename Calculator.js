c8b04cd7b05651d1728d6e0712fdc44e2e4ea058
Brent Cameron <bjcameron@gmail.com>
2020-05-04 15:01:14 -0600

    got the number input functionality working

--- Calculator.js
+++ Calculator.js
@@ -1,20 +1,3 @@
-
-//this should handle everything that comes up on the display.
-function Display() {
-    let display = document.querySelector('.calculator-display');
-    //display.innerText = '123';
-    console.log(display);
-}
-
-function ButtonPress (whichButton){
-    if(isNaN(whichButton)){
-        //then its not a number...
-    }else {
-        //it is a number and should be concatenated to the previous number if there are any.
-    }
-    console.log(whichButton);
-}
-
 //this is where ill push all the new button press info
 //once we see an '=' then we will have to evaluate the elements of the stack using BEDMAS
 class Stack {
@@ -51,6 +34,75 @@ class Stack {
     }
 }
 
+
+//this should handle everything that comes up on the display.
+function Display(buttonStack) {
+    let display = document.querySelector('.calculator-display');
+    display.innerText = buttonStack.peek();
+    //console.log(display);
+}
+
+function ButtonPress (whichButton, buttonStack){
+    console.log('printing stack ' + buttonStack.print());
+    console.log('stack length is '  + buttonStack.length());
+    if(isNaN(whichButton)){
+        //then its not a number...
+        // if(whichButton === '='){
+        //     //then we have to evaluate all the stack.
+        // }
+        switch(whichButton){
+            case '=':
+
+                break;
+
+            case 'clear':
+                let cleanButtonStack = Clear();
+                Display(cleanButtonStack);
+                break;
+            case 'backspace': 
+                break;
+            case '.':
+                alert('i dont do decimals yet...');
+                break;
+
+            case '+':
+
+                break;
+
+            case '-':
+                
+                break;
+
+            case '/':
+
+                break;
+
+            case '*':
+
+                break;
+
+
+        }
+    }else {
+        //it is a number and should be concatenated to the previous number if there are any.
+        // if(buttonStack.peek()){
+        //     console.log(buttonStack.peek());
+        if(isNaN(buttonStack.peek())){
+            //then the previously pressed button was not a number.
+            // and should not do the concatenation.
+        }else{
+            //have to do this conversion between int/string/int so that it concatenates rather than adding together.
+            let concatenatedNum = "" + buttonStack.pop() + whichButton;
+            buttonStack.push(parseInt(concatenatedNum));
+            console.log(`pushing ${concatenatedNum} into the stack`);        
+        }
+
+    }
+    Display(buttonStack);
+}
+
+
+
 function Addition (a, b) {
     return (parseInt(a) + parseInt(b));
 }
@@ -67,40 +119,47 @@ function Multiplication (a, b){
     return (parseInt(a) * parseInt(b));
 }
 
+//this should give us a fresh stack to start pushing things into.
 function Clear () {
     let buttonStack = new Stack ();
+    buttonStack.push('');
+    console.log(buttonStack.peek());
     return(buttonStack);
 }
 
-Display();
+
+//create new stack
+let buttonStack = Clear();
+//run the display.
+Display(buttonStack);
 
 // button listeners below:
 
 //numbers in order they appear on the screen
-let seven = document.getElementById('seven').addEventListener('click', () => ButtonPress(7));
-let eight = document.getElementById('eight').addEventListener('click', () =>ButtonPress(8));
-let nine = document.getElementById('nine').addEventListener('click', () => ButtonPress(9));
+let seven = document.getElementById('seven').addEventListener('click', () => ButtonPress(7, buttonStack));
+let eight = document.getElementById('eight').addEventListener('click', () =>ButtonPress(8, buttonStack));
+let nine = document.getElementById('nine').addEventListener('click', () => ButtonPress(9, buttonStack));
 
-let four = document.getElementById('four').addEventListener('click', () => ButtonPress(4));
-let five = document.getElementById('five').addEventListener('click', () =>ButtonPress(5));
-let six = document.getElementById('six').addEventListener('click', () =>ButtonPress(6));
+let four = document.getElementById('four').addEventListener('click', () => ButtonPress(4, buttonStack));
+let five = document.getElementById('five').addEventListener('click', () =>ButtonPress(5, buttonStack));
+let six = document.getElementById('six').addEventListener('click', () =>ButtonPress(6, buttonStack));
 
-let one = document.getElementById('one').addEventListener('click', () => ButtonPress(1));
-let two = document.getElementById('two').addEventListener('click', () => ButtonPress(2));
-let three = document.getElementById('three').addEventListener('click', () => ButtonPress(3));
+let one = document.getElementById('one').addEventListener('click', () => ButtonPress(1, buttonStack));
+let two = document.getElementById('two').addEventListener('click', () => ButtonPress(2, buttonStack));
+let three = document.getElementById('three').addEventListener('click', () => ButtonPress(3, buttonStack));
 
-let zero = document.getElementById('zero').addEventListener('click', () => ButtonPress(0));
+let zero = document.getElementById('zero').addEventListener('click', () => ButtonPress(0, buttonStack));
 
 //operators in order they appear on the screen.
 
-let divide = document.getElementById('divide').addEventListener('click', () => ButtonPress('/'));
-let multiply = document.getElementById('multiply').addEventListener('click', () => ButtonPress('*'));
-let subtract = document.getElementById('subtract').addEventListener('click', () => ButtonPress('-'));
-let add = document.getElementById('add').addEventListener('click', () => ButtonPress('+'));
+let divide = document.getElementById('divide').addEventListener('click', () => ButtonPress('/', buttonStack));
+let multiply = document.getElementById('multiply').addEventListener('click', () => ButtonPress('*', buttonStack));
+let subtract = document.getElementById('subtract').addEventListener('click', () => ButtonPress('-', buttonStack));
+let add = document.getElementById('add').addEventListener('click', () => ButtonPress('+', buttonStack));
 
 //all other buttons below:
 
-let decimal  = document.getElementById('decimal').addEventListener('click', () => ButtonPress('.'));
-let backSpace  = document.getElementById('backspace').addEventListener('click', () => ButtonPress('backspace'));
-let clear  = document.getElementById('clear').addEventListener('click', () => ButtonPress('clear'));
-let equals  = document.getElementById('equals').addEventListener('click', () => ButtonPress('='));
+let decimal  = document.getElementById('decimal').addEventListener('click', () => ButtonPress('.', buttonStack));
+let backSpace  = document.getElementById('backspace').addEventListener('click', () => ButtonPress('backspace', buttonStack));
+let clear  = document.getElementById('clear').addEventListener('click', () => ButtonPress('clear', buttonStack));
+let equals  = document.getElementById('equals').addEventListener('click', () => ButtonPress('=', buttonStack));
