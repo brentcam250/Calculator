a96851ff0f7ec124fe3f335ccb385ca2df265190
Brent Cameron <bjcameron@gmail.com>
2020-05-07 19:42:28 -0600

    fixed the multiple operators logic, broke backspace and clear in the process

--- Calculator.js
+++ Calculator.js
@@ -22,6 +22,7 @@ class Stack {
             return this.data[this.top-2];
         }
     }
+
     isEmpty() {
         return this.top === 0;
     }
@@ -39,32 +40,38 @@ class Stack {
             top --;
         }
     }
+
     clear() {
         //destroys everything in the stack... be careful.
         while(this.isEmpty() === false){
             this.pop();
         }
-        this.push('');
     }
 }
 
 // this takes in the current stack, and will evaluate 
-function Evaluate(buttonStack){
+// function Evaluate(buttonStack){
+function Evaluate(){
+
     //lets see what we're working with.
     buttonStack.print();
     let result;
     let lastIn;
     let operator;
     let firstIn;
-    if(buttonStack.length() >= 3){
+    if(buttonStack.length() === 3){
         lastIn = buttonStack.pop();
         operator = buttonStack.pop();
         firstIn = buttonStack.pop();
         if(isNaN(lastIn) || isNaN(firstIn)){
-            console.log('something is wrong... i think that two operators may have been pushed back to back, may also get this message if operator then backspace pressed');
+            console.log('something is wrong... i think that two operators may have been pushed back to back,' + 
+            ' may also get this message if operator then backspace pressed');
         }
     }
     else{
+        //dont have enough things to evaluate.
+        console.log("not exactly 3 things in my stack so ill print to show you ");
+        buttonStack.print();
         return 0;
     }
 
@@ -82,14 +89,11 @@ function Evaluate(buttonStack){
             break;
 
         case '/':
-            console.log("im about to call the divison method");
             result = Division(firstIn, lastIn);
-            console.log(`result of division is ${result}`);
-            buttonStack.print();
-            console.log('length of stack is ' +  buttonStack.length())
             break;
     }
     buttonStack.push(result);
+
 
 
     return 1;
@@ -97,18 +101,19 @@ function Evaluate(buttonStack){
 
 
 //this should handle everything that comes up on the display.
-function Display(buttonStack) {
+//function Display(buttonStack) {
+function Display(){
     let display = document.querySelector('.calculator-display');
-    if(buttonStack.peek() === '' ){
+    if(buttonStack.peek() === undefined){
         // this placeholder keeps the display the same size.
-        display.innerText = '|';
+        display.innerText = 'Nothing to see here...';
         display.style.color = '#AAAAAE';
     }else{
         display.style.color = 'white';
         if(isNaN(buttonStack.peek())){
             //want to ignore displaying operators on the screen. peek 2 deep to get the last number.
             display.innerText = buttonStack.peekTwo();
-        }else{
+        }else {
             display.innerText = buttonStack.peek();
         }
         
@@ -117,37 +122,43 @@ function Display(buttonStack) {
 }
 
 
-function ButtonPress (whichButton, buttonStack){
+//function ButtonPress (whichButton, buttonStack){
+function ButtonPress (whichButton){
+
     //if we have a full expression and another operator is pressed, we want to evaluate the first part before moving on.
-    if(buttonStack.length()>3){
-        console.log('im doing the multiple operator eval method');
-        //buttonStack.print();
-        let savedOperator = buttonStack.pop();
+    if(buttonStack.length() >= 3 && isNaN(whichButton)){
         let didEval = Evaluate(buttonStack);
         if(!didEval){
             console.log('received error from evaluate');
         }
-        //Display(buttonStack);
-        buttonStack.push(savedOperator);
+        
+        Display();
+        buttonStack.push(whichButton);
+
     }
     if(isNaN(whichButton)){
         if(isNaN(buttonStack.peek())){
             //don't want two operators in a row...
+            //this gets weird when we want to clear or backspace
             return null;
         }
         switch(whichButton){
             case '=':
-                Evaluate(buttonStack);
+                let didEval = Evaluate(buttonStack);
+                if(!didEval){
+                    console.log('received an error from evaluate');
+                }
                 break;
 
             case 'clear':
                 buttonStack.clear();
                 break;
             case 'backspace': 
-                doBackSpace(buttonStack);
+                let backspaceResult = doBackSpace(buttonStack);
                 //this deletes the last single character input
                 //for example it would delete a + or a *, but if it were a number input such as 623,
                 //backspace would only delete the last character leaving us 62
+                console.log(backspaceResult);
                 break;
             case '.':
                 alert('i dont do decimals yet...');
@@ -160,8 +171,6 @@ function ButtonPress (whichButton, buttonStack){
         }
     }else {
         //it is a number and should be concatenated to the previous number if there are any.
-        // if(buttonStack.peek()){
-        //     console.log(buttonStack.peek());
         if(isNaN(buttonStack.peek())){
             //then the previously pressed button was not a number.
             // and should not do the concatenation.
@@ -169,8 +178,7 @@ function ButtonPress (whichButton, buttonStack){
         }else{
             //have to do this conversion between int/string/int so that it concatenates rather than adding together.
             let concatenatedNum = "" + buttonStack.pop() + whichButton;
-            buttonStack.push(parseInt(concatenatedNum));
-            //console.log(`pushing ${concatenatedNum} into the stack`);        
+            buttonStack.push(parseInt(concatenatedNum));   
         }
 
     }
@@ -185,8 +193,8 @@ function Addition (a, b) {
     b = parseFloat(b);
 
     let result = a + b;
-    result = Math.round((result * 100 )/100);
-    return result;
+    result = Math.round((result * 100 ));
+    return result/100;
 
 }
 
@@ -195,22 +203,22 @@ function Subtraction (a, b) {
     b = parseFloat(b);
 
     let result = a - b;
-    result = Math.round((result * 100 )/100);
-    return (result);
+    result = Math.round((result * 100 ));
+    return (result /100);
 }
 
 function Division (a, b) {
     a = parseFloat(a);
     b = parseFloat(b);
-    console.log(`im about to attempt dividing  ${a} / ${b}`);
     if(b === 0){
-        console.log('b is zero');
         //dont divide by zero plz
-        return 'You suck';
+        alert("You suck, dont divide by 0");
+        return '';
     }
     let result = a / b;
-    //result = Math.round((result * 100 )/100);
-    return (result);
+    result = Math.round((result * 100 ));
+    // not sure why this didnt want to work when the *100 and /100 were in the same line... 
+    return (result/ 100);
 }
 
 function Multiplication (a, b){
@@ -218,17 +226,19 @@ function Multiplication (a, b){
     b = parseFloat(b);
 
     let result = a * b;
-    result = Math.round((result * 100 )/100);
-    return (result);
+    result = Math.round((result * 100 ));
+    return (result /100);
 }
 
 //returnss 0 if it took off an operator
 //returns 1 if it shortened a number
-function doBackSpace(buttonStack){
+// function doBackSpace(buttonStack){
+function doBackSpace(){
+
     let prev = buttonStack.peek();
     if(isNaN(prev)){
         //last thing is not a number so just pop it off and forget about it.
-        buttonStack.pop();
+        console.log('deleting '  + buttonStack.pop());
         return 0;
     }else{
         console.log(prev);
@@ -243,15 +253,14 @@ function doBackSpace(buttonStack){
 //this should give us a fresh stack to start pushing things into.
 function Clear () {
     let buttonStack = new Stack ();
-    buttonStack.push('');
     console.log(buttonStack.peek());
     return(buttonStack);
 }
 //create a new stack, and setup the calculator for input
 let buttonStack = new Stack ();
-buttonStack.push('');
+
 //run the display.
-Display(buttonStack);
+Display();
 
 // button listeners below:
 
